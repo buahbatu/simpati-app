debc99a703c6dd28b1423295c6dcc69128af8841
alifgiant <muh.alifgiant@gmail.com>
2020-02-02 17:43:57 +0700

    add app bar to add page

--- mobile/lib/presentation/kid/page/add_page.dart
+++ mobile/lib/presentation/kid/page/add_page.dart
@@ -4,32 +4,14 @@ class KidAddPage extends StatelessWidget {
   Widget createAppBar(BuildContext context) {
     return AppBar(
       elevation: 0,
-
-      // title: Wrap(
-      //   direction: Axis.vertical,
-      //   spacing: 2,
-      //   children: <Widget>[
-      //     Text(
-      //       'Daftar Ibu',
-      //       style: AppTextStyle.title.copyWith(
-      //         color: AppColor.primaryColor,
-      //         fontSize: 16,
-      //       ),
-      //     ),
-      //     Text(
-      //       '300 Orang',
-      //       style: AppTextStyle.titleName.copyWith(fontSize: 12),
-      //     ),
-      //   ],
-      // ),
-      // backgroundColor: AppColor.appBackground,
-      // actions: <Widget>[
-      //   IconButton(
-      //     icon: Icon(Icons.search),
-      //     color: AppColor.primaryColor,
-      //     onPressed: () => context.showComingSoonNotice(),
-      //   )
-      // ],
+      title: Text('Tambah Anak'),
+      actions: <Widget>[
+        FlatButton(
+          textColor: Colors.white,
+          child: Text('Simpan'),
+          onPressed: () {},
+        )
+      ],
     );
   }
 
--- mobile/lib/presentation/mother/page/add_page.dart
+++ mobile/lib/presentation/mother/page/add_page.dart
@@ -4,32 +4,14 @@ class MotherAddPage extends StatelessWidget {
   Widget createAppBar(BuildContext context) {
     return AppBar(
       elevation: 0,
-
-      // title: Wrap(
-      //   direction: Axis.vertical,
-      //   spacing: 2,
-      //   children: <Widget>[
-      //     Text(
-      //       'Daftar Ibu',
-      //       style: AppTextStyle.title.copyWith(
-      //         color: AppColor.primaryColor,
-      //         fontSize: 16,
-      //       ),
-      //     ),
-      //     Text(
-      //       '300 Orang',
-      //       style: AppTextStyle.titleName.copyWith(fontSize: 12),
-      //     ),
-      //   ],
-      // ),
-      // backgroundColor: AppColor.appBackground,
-      // actions: <Widget>[
-      //   IconButton(
-      //     icon: Icon(Icons.search),
-      //     color: AppColor.primaryColor,
-      //     onPressed: () => context.showComingSoonNotice(),
-      //   )
-      // ],
+      title: Text('Tambah Ibu'),
+      actions: <Widget>[
+        FlatButton(
+          textColor: Colors.white,
+          child: Text('Simpan'),
+          onPressed: () {},
+        )
+      ],
     );
   }
 
