2feb8397cd0905caab4dd73d0945f288ee753d33
Alif Akbar <muh.alifgiant@gmail.com>
2020-07-09 14:48:48 +0800

    refactor main to use new home

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,21 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:simpati/core/tools/app_loader.dart';
-import 'package:simpati/core/resources/app_color.dart';
-import 'package:simpati/presentation/app/app_bloc.dart';
-import 'package:simpati/presentation/home/screen.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-
-bool isDebug = false;
+import 'package:simpati/core/resources/res_color.dart';
+import 'package:simpati/feature/home/home_screen.dart';
 
 void main() {
-  runZoned<Future<void>>(() async {
-    // run on splash screen
-    await AppLoader.get().onAppStart(isDebug: isDebug);
-
-    WidgetsFlutterBinding.ensureInitialized();
-    runApp(MyApp());
-  }, onError: Crashlytics.instance.recordError);
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (ctx) => AppBloc(isDebug: isDebug)..add(AppEvent.AppLoaded),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(primaryColor: AppColor.primaryColor),
-        home: HomeScreen(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: ResColor.primaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: HomeScreen(),
     );
   }
 }
